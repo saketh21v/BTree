f4f178e995c3706923843f07d10841a4fb4fac4c
FrozenMatrix <vsaketh@yahoo.co.in>
2016-09-04 16:10:15 +0530

    Working project. Scalable. Needs performance improvements. First push to GitHub

---
+++ src/btreeold/BTree.java
@@ -0,0 +1,275 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btreeold;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Saketh
+ */
+public class BTree {
+
+    public final static int INVALID = -1;
+    public final static int ORDER = 5;
+    public final static int DEGREE = ORDER - 1;
+    private Node root;
+    private final int initLevel = 1;
+    private int highestLevel = initLevel;
+
+    BTree(int val) {
+        this.root = new Node();
+        this.root.keys[0] = val;
+    }
+
+    public int getHighestLevel() {
+        return highestLevel;
+    }
+
+    private void updateAndResetLevels() {
+        updateEmptyNodes(this.root, initLevel);
+        resetLevels(this.root, initLevel);
+    }
+
+    private void updateEmptyNodes(Node root, int h) {
+        if (h == this.highestLevel) {
+            return;
+        }
+
+        for (int i = 0; i < root.childs.length; ++i) {
+            if (root.childs[i] == null) {
+                root.childs[i] = new Node();
+            }
+        }
+        for (Node x : root.childs) {
+            updateEmptyNodes(x, h + 1);
+        }
+
+    }
+
+    private void resetLevels(Node root, int h) {
+        if (h == this.highestLevel || Node.isLeaf(root)) {
+            return;
+        }
+//        if(Node.isLeaf(root))
+//            return;
+        for (int i = 0; i < root.keys.length; i++) {
+            if (root.keys[i] == INVALID) {
+                //Node.resetNodeChilds(root.childs[i]);
+                Node.resetNodeChilds(root.childs[i + 1]);
+            }
+        }
+        if (root.keys[0] == INVALID) {
+            Node.resetNodeChilds(root.childs[0]);
+            return;
+        }
+        for (Node x : root.childs) {
+            resetLevels(x, h + 1);
+        }
+    }
+
+    public static BTree createBTree(int val) {
+        BTree btree = new BTree(val);
+        return btree;
+    }
+
+    public boolean insert(int val) {
+//        System.out.println("The value " + val + " is being inserted");
+        if(this.searchVal(val)){
+//            System.err.println("Value already exists");
+            return false;
+        }
+        
+        
+        Node temp = this.root;
+        this.root = this.insertVal(val, root);
+        if(temp != this.root) this.highestLevel++;
+//        this.updateHLevel(this.root, 0);
+        updateAndResetLevels();
+//        System.out.println("The value " + val + " is inserted @ " + this.root + " node.keyCount = " + this.root.keyCount);
+//        Node.printNode(this.root);
+        //Node.printNode(this.root.childs[0]);
+        //Node.printNode(this.root.childs[1]);
+        return true;
+    }
+
+    private static Node insertVal(int val, Node node) {
+        //Making sure all the counts are proper.
+        if(node == null) return null;
+        node.updateChildCount();
+        node.updateKeyCount();
+
+        if (Node.isLeaf(node)) { // Checking if leaf node. CASE 1: Leaf Node.
+            if (!node.isFull()) { //Leaf node not full case.
+                node.insertVal(val);
+                return node;
+            } else { //Leaf node full. Split and send temporary parent to calling function
+                return splitNode(val, null, node);
+            }
+        } else {// CASE 2 : NON-Leaf node 
+            Node tParent, lNode;// tParent -> Temporary parent, lNode -> Leaf Node(Assumption. Harmless)
+            int pos = 0;
+            for (pos = 0; pos < DEGREE; ++pos) {
+                if (node.keys[pos] == INVALID) {
+                    break;
+                }
+                if (node.keys[pos] > val) {
+                    break;
+                }
+            }
+            lNode = node.childs[pos];
+            tParent = insertVal(val, lNode);
+            if (tParent == lNode) // SUBCASE 1: CHILD NOT FULL. Inserted in child node since it wasn't full. No worries.
+            {
+                return node;
+            }
+            //SUBCASE 2: CHILD FULL. Got a tParent.
+            // tParent not equal to lNode means that leaf node was full and has been split.
+
+            int midVal = tParent.keys[0];
+            node.updateChildCount();
+            node.updateKeyCount();
+            if (!node.isFull()) { //(;p)SUB2-SUBCASE 1: Current node is NOT FULL
+                int midValPos = 0;
+                for (midValPos = 0; midValPos < DEGREE; ++midValPos) {
+                    if (node.keys[midValPos] == INVALID) {
+                        break;
+                    }
+                    if (node.keys[midValPos] > midVal) {
+                        break;
+                    }
+                }
+                node.shiftByOneAt(midValPos, true);
+                node.keys[midValPos] = midVal;
+                node.childs[midValPos + 1] = tParent.childs[1];
+                return node;
+            } else { // SUB2-SUBCASE 2: Current node is FULL. Delegate to Parent
+                tParent = splitNode(midVal, tParent, node);
+                return tParent;
+            }
+        }
+    }
+
+    private static Node splitNode(int val, Node midNode, Node node) {
+        Node tParent = new Node(); // Temporary Parent
+        Node sibl = new Node();
+        int midPos = ORDER / 2;
+
+        int[] arr = new int[DEGREE + 1];
+        Node[] arrN = new Node[ORDER + 1];
+        for (int i = 0; i < node.keys.length; ++i) {
+            arr[i] = node.keys[i];
+        }
+        int j = 0;
+        int c = 0; // Used to find where to put the childs of midNode.
+        arr[ORDER - 1] = val;
+        Arrays.sort(arr);
+        for (int i = 0; i < arr.length; ++i) {
+            if (arr[i] == val) {
+                c = i;
+                break;
+            }
+        }
+        int KKK = 0;
+        if (val == 665) {
+            KKK = 1;
+        }
+        if (midNode != null) {
+            arrN[c] = midNode.childs[0];
+            arrN[c + 1] = midNode.childs[1];
+        }
+        j = 0; int KK;
+        for (Node x : node.childs) {
+            if ((j == c)) {
+                j++;
+                continue;
+            }
+            if ((j == c + 1)) {
+                j++;
+                arrN[j++] = x;
+                continue;
+            }
+            arrN[j] = x;
+            ++j;
+        }
+        Node.resetNodeKeys(node);
+        if(!Node.isLeaf(node))
+            Node.resetNodeChilds(node);
+        for (int i = 0; i < DEGREE / 2; ++i) {
+            node.keys[i] = arr[i];
+            sibl.keys[i] = arr[midPos + i + 1];
+        }
+        //Updating the childs of all the nodes : node, sibl and tParent
+        if (midNode != null) {
+            for (int i = 0; i <= DEGREE / 2; ++i) {
+                node.childs[i] = arrN[i];
+            }
+            j = 0;
+            int ii = 0;
+            for (int i = ((ORDER / 2) + 1); i < (ORDER + 1); ++i) {
+                sibl.childs[j++] = arrN[i];
+            }
+        }
+
+        node.updateKeyCount();
+        sibl.updateKeyCount();
+
+        tParent.keys[0] = arr[midPos];
+        tParent.childs[0] = node;
+        tParent.childs[1] = sibl;
+
+        for (int i = 2;
+                i < ORDER;
+                ++i) {
+            tParent.childs[i] = new Node();
+        }
+
+        tParent.updateKeyCount();
+
+        tParent.updateChildCount();
+
+        return tParent;
+    }
+
+    public void printBTree() {
+        this.printTree(this.root, 0);
+    }
+
+    private void printTree(Node root, int i) {
+        if (root == null || root.isEmpty()) {
+            return;
+        }
+        System.out.print("Level " + i + " : ");
+        for (int x : root.keys) {
+            System.out.print(x + " ");
+        }
+        System.out.println();
+        for (Node x : root.childs) {
+            printTree(x, i + 1);
+        }
+    }
+
+    public boolean searchVal(int val) {
+        return search(val, this.root);
+    }
+
+    private static boolean search(int key, Node root) {
+        if(root == null) return false;
+        if(root.isEmpty()) return false;
+        int i=0;
+        for(i=0;i<DEGREE;++i){
+            if(root.keys[i] == INVALID)
+                break;
+            if(root.keys[i] == key)
+                return true;
+            if(root.keys[i] > key)
+                return search(key, root.childs[i]);
+        }
+//        if(root.keys[i] == INVALID)
+//            return search(key, root.childs[i]);
+        return search(key, root.childs[i]);
+    }
+}
---
+++ src/btreeold/Node.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package btreeold;
+
+import static btreeold.BTree.DEGREE;
+import static btreeold.BTree.INVALID;
+import static btreeold.BTree.ORDER;
+
+/**
+ *
+ * @author Saketh
+ */
+public class Node {
+    int[] keys = new int[DEGREE];
+    Node[] childs = new Node[ORDER];
+    private int keyCount = 0;
+    private int childCount = 0;
+    public int level = 0;
+    Node() {
+        this.level = level;
+        for (int i = 0; i < keys.length; ++i) {
+            keys[i] = INVALID;
+        }
+        for (int i = 0; i < childs.length; ++i) {
+            childs[i] = null;
+        }
+    }
+
+    boolean isFull() {
+        return (keyCount == DEGREE);
+    }
+    
+    boolean isEmpty(){
+        return (keyCount == 0);
+    }
+
+    void insertVal(int val) {
+//        System.out.println("INSERTVAL : Val = "+val+" isLeaf = "+isLeaf(this)+" Node = "+this);
+        int j = 0;
+        for (; j < keys.length; ++j) {
+            if (keys[j] == INVALID) {
+                break;
+            }
+            if (keys[j] > val) {
+                break;
+            }
+        }
+        for (int i = (keys.length - 2); i >= j; --i) {
+            keys[i + 1] = keys[i];
+        }
+        keys[j] = val;
+        this.updateKeyCount();
+    }
+
+    void updateKeyCount() {
+        int j = 0;
+        for (; j < keys.length; ++j) {
+            if (keys[j] == INVALID) {
+                break;
+            }
+        }
+        this.keyCount = j;
+    }
+
+    void updateChildCount() {
+        this.childCount = 0;
+        for (int j = 0; j < childs.length; ++j) {
+            if (childs[j] != null) {
+                ++this.childCount;
+            }
+        }
+    }
+
+    void shiftByOneAt(int pos, boolean right) {
+        if (!(pos < DEGREE)) {
+            System.err.println("ERRROR : pos is greater than DEGREE");
+            System.exit(0);
+        }
+        if (right) {
+            int shiftToPos = 0;
+            for (shiftToPos = pos; shiftToPos < DEGREE; ++shiftToPos) {
+                if (this.keys[shiftToPos] == -1) {
+                    break;
+                }
+            }
+            for (int i = shiftToPos; i > pos; --i) {
+                this.keys[i] = this.keys[i - 1];
+                this.childs[i + 1] = this.childs[i];
+            }
+        } else {
+            int shiftToPos = 0;
+            for (shiftToPos = pos; shiftToPos >= 0; --shiftToPos) {
+                if (this.keys[shiftToPos] == -1) {
+                    break;
+                }
+            }
+            for (int i = shiftToPos; i < pos; ++i) {
+                this.keys[i] = this.keys[i + 1];
+                this.childs[i - 1] = this.childs[i];
+            }
+        }
+    }
+
+    static void resetNodeKeys(Node node) {
+        for (int i = 0; i < node.keys.length; ++i) {
+            node.keys[i] = INVALID;
+        }
+    }
+    
+    static void resetNodeChilds(Node node){
+        if(Node.isLeaf(node)) return;
+        for(int i = 0;i<node.childs.length;++i)
+            node.childs[i] = new Node();
+    }
+
+    static boolean isLeaf(Node node) {
+        return (node.childCount == 0);
+    }
+
+    static void printNode(Node node) {
+        if (node == null) {
+            return;
+        }
+        for (int i = 0; i < node.keys.length; ++i) {
+            System.out.printf(node.keys[i] + " ");
+        }
+        System.out.println();
+    }
+}
